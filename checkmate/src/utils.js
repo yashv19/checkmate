2c7430a1504eca57a985d2461015158c88b7420f
Yashvin Vedanaparti <yvedanaparti@gmail.com>
2024-10-08 22:58:27 -0400

    More random note intros

--- checkmate/src/utils.js
+++ checkmate/src/utils.js
@@ -1,13 +1,26 @@
 
 export const getRandomPlaceholder = () => {
     const randomPlaceholders = [
-        'Once upon a time...',
-        'Today I want to...',
-        'I discovered that...',
+        `Once upon a time...`,
+        `Today I want to...`,
+        `I discovered that...`,
         `I can't stop wondering about...`,
         `What excites me most right now is...`,
         `A challenge I'm facing right now is...`,
-        `I've been thinking a lot about...`
+        `I've been thinking a lot about...`,
+        `One thing I'm grateful for today is...`,
+        `If I could change one thing, it would be...`,
+        `A goal I want to achieve is...`,
+        `Something that surprised me recently...`,
+        `I've always wanted to learn...`,
+        `An important lesson I learned is...`,
+        `The best advice I received was...`,
+        `If I could travel anywhere, I'd go to...`,
+        `Something that made me smile today...`,
+        `One thing I can improve on is...`,
+        `If money wasn't a factor, I would...`,
+        `A person I admire is...`,
+        `I'm excited about the future because...`
     ];
 
     const random = Math.floor(Math.random() * randomPlaceholders.length);
